106f182719ae214093d2fca6923cfad69b906fd1
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-09-18 20:59:36 -0300

    criando as regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
@@ -22,6 +23,34 @@ export class QuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionsSelected = this.questions[this.questionIndex]
+
+      this.questionMaxIndex = this.questions.length
+      this.questionIndex = 0
+
+    }
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.questionIndex
+    this.nextStep()
+  }
+  nextStep(){
+      this.questionIndex += 1
+      if (this.questionIndex < this.questionMaxIndex){
+        this.questionsSelected = this.questions[this.questionIndex]
+      }else {
+        this.finished = true
+
+      }
+
   }
 
 }
